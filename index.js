6b25ede492bf60e24ddd9cb3a9691c9ee729e487
Daniel <curveballdaniel@gmail.com>
2020-01-28 14:02:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+'use strict';
+
+const express = require('express');
+
+const app = express();
+
+if (module === require.main) {
+  // [START server]
+  // Start the server
   const server = app.listen(process.env.PORT || 8080, () => {
     const port = server.address().port;
     console.log(`App listening on port ${port}`);
   });
+  // [END server]
+}
+
+module.exports = app;
